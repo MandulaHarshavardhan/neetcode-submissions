a00c9a33537fdec0204a1c0936374419391122ea
Mandula Harshavardhan <charyharshavardhan@gmail.com>
2026-08-03 19:57:19 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        # dici={}
+        # for i in nums:
+        #     if i not in dici:
+        #         dici[i]=1
+        #     else:
+        #         return True
+        # return False  
+        hashset=set()
+        for i in nums:
+            if i not in hashset:
+                hashset.add(i)
+            else:
+                return True
+        return False                   
